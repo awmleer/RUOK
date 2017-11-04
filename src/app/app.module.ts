f31335d02c88269df2e3338de8f32fbab9038653
awmleer <awmleer@sparker.xyz>
2017-11-04 20:06:20 +0800

    fix css style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HttpModule} from '@angular/http';
+import { SanitizeHtmlPipe } from './sanitize-html.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SanitizeHtmlPipe
   ],
   imports: [
     BrowserModule,
