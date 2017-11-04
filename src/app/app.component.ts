fdca0fe389f40e8224f56ba4c3e1bab2db55d8e1
awmleer <awmleer@sparker.xyz>
2017-11-04 16:48:31 +0800

    record button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  recording:boolean=false;
+
+
+  toggleRecord(){
+    this.recording=!this.recording;
+  }
+
 }
