437c92219f7a2e280aaf6f40c3e43a9d325efb76
awmleer <awmleer@sparker.xyz>
2017-11-04 18:19:49 +0800

    fallback to python backend record audio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
@@ -8,68 +10,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   recording:boolean=false;
-  // recordRTC=null;
-  recorder=null;
-  blob=null;
 
-
-
-  ngOnInit(){
-    // let session = {
-    //   audio: true,
-    //   video: false
-    // };
-    // navigator.getUserMedia(session, (mediaStream)=>{
-    //   this.recordRTC = eval('RecordRTC(MediaStream)');
-    // }, ()=>{});
-  }
+  constructor(
+    private http: Http
+  ){}
 
   toggleRecord(){
-    // let session = {
-    //   audio: true,
-    //   video: false
-    // };
     if (this.recording) {
-      // this.recorder.stop();
-    }else{
-      function startUserMedia(stream) {
-        var input = audio_context.createMediaStreamSource(stream);
-        __log('Media stream created.');
-        // Uncomment if you want the audio to feedback directly
-        //input.connect(audio_context.destination);
-        //__log('Input connected to audio context destination.');
-
-        recorder = new Recorder(input);
-        __log('Recorder initialised.');
-      }
-      navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
-        __log('No live audio input: ' + e);
+      this.http.get('http://localhost:8000/done/').toPromise().then(() => {
+          this.recording=false;
       });
-      // navigator.getUserMedia(session, (mediaStream)=>{
-      //   // this.recordRTC = eval('RecordRTC(MediaStream)');
-      //   this.recorder = new window['MediaRecorder'](mediaStream);
-      //   this.recorder.ondataavailable = (event)=>{
-      //     if (this.recorder.state === 'recording') {
-      //       this.blob = new window.Blob([event.data], {
-      //         type: 'audio/ogg'
-      //       });
-      //       // use the created blob
-      //
-      //       console.log(this.blob);
-      //     }
-      //   };
-      //
-      //   this.recorder.onstop = function() {
-      //     this.recorder = null;
-      //     console.log(this.blob);
-      //   };
-      //
-      //   this.recorder.start(length);
-      // }, ()=>{});
+    }else{
+      this.http.get('http://localhost:8000/start/').toPromise().then(() => {
+        this.recording=true;
+      });
     }
-    this.recording=!this.recording;
   }
-
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
